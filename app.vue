ab8367f2bdf6ef033f1b530757196244ff98fa14
Luis Sena <luis.senarode@gmail.com>
2023-12-13 18:50:40 -0300

    feat: :lipstick: layout margin center

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <img class="mx-auto py-5" src="/faster_logo.png" />
   </header>
 
-  <main class="px-8 lg:max-w-7xl lg:mx-auto py-12">
+  <main class="py-12 px-8 mx-auto lg:max-w-7xl">
     <NuxtPage />
   </main>
 </template>
