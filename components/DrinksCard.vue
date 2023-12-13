58f86dac41523793604ba4bc5742059787b10351
Luis Sena <luis.senarode@gmail.com>
2023-12-13 14:08:03 -0300

    feat: :sparkles: drinks card

---
+++ components/DrinksCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface IDrinksCardProps {
+  id: string
+  name: string
+  image: string
+}
+
+const props = defineProps<IDrinksCardProps>()
+</script>
+
+<template>
+  <div
+    class="flex flex-col gap-2 border rounded-xl hover:shadow cursor-pointer"
+  >
+    <img class="rounded-t-lg" :src="props.image" alt="drink" />
+    <h1 class="mx-auto text-center p-2">{{ props.name }}</h1>
+  </div>
+</template>
