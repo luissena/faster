ef807cf4406d5a456020ce182c234033a3e025ca
Luis Sena <luis.senarode@gmail.com>
2023-12-13 14:09:49 -0300

    feat: :speech_balloon: empty drinks

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
   <DrinksHeader />
+
+  <p class="text-center text-lg text-gray-400">
+    Nenhuma bebida encontrada. Por favor, selecione uma categoria ou busque por
+    um nome.
+  </p>
 </template>
