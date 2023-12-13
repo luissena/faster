45f65b53d50dd7e05cf8a9b03adcb27870f42c4d
Luis Sena <luis.senarode@gmail.com>
2023-12-13 11:19:45 -0300

    feat: :sparkles: drinks category filter

---
+++ components/DrinksCategoryFilter.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+interface ICategoryData {
+  drinks: {
+    strCategory: string
+  }[]
+}
+
+const { data } = await useFetch<ICategoryData>(
+  "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+)
+
+const categorySelected = ref("")
+</script>
+
+<template>
+  <select
+    class="border border-gray-300 rounded-md px-4 py-2 text-gray-400"
+    v-model="categorySelected"
+  >
+    <option disabled value="">Categorias</option>
+    <option v-for="category in data?.drinks">{{ category.strCategory }}</option>
+  </select>
+</template>
