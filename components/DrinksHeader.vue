b69fc82e0fe5a77fc8e6e8d1de1cff24bc3beb26
Luis Sena <luis.senarode@gmail.com>
2023-12-13 11:20:08 -0300

    feat: :sparkles: drinks header

---
+++ components/DrinksHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header
+    class="flex flex-col gap-4 lg:flex-row justify-between lg:items-center"
+  >
+    <div class="flex gap-2 items-center">
+      <Icon size="30px" name="bx:drink" />
+      <h1 class="text-3xl font-bold text-gray-800">Bebidas</h1>
+    </div>
+
+    <div class="flex gap-4">
+      <DrinksCategoryFilter />
+      <DrinksSearchByName />
+    </div>
+  </header>
+</template>
---
+++ components/DrinksSearchByName.vue
@@ -0,0 +1,7 @@
+<template>
+  <input
+    type="text"
+    class="border border-gray-300 rounded-md px-4 py-2"
+    placeholder="Buscar por nome"
+  />
+</template>
