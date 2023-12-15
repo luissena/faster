64ba164e70e0e105e9169bda066bbb9a9d3734bc
Luis Sena <luis.senarode@gmail.com>
2023-12-15 17:02:47 -0300

    feat: :goal_net: custom error page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <img class="mx-auto py-5" src="/faster_logo.png" />
+  </header>
+
+  <main class="py-12 mx-auto">
+    <div class="flex flex-col items-center justify-center">
+      <h1 class="text-3xl font-bold text-gray-700">Error</h1>
+      <p class="text-gray-600">
+        An error occurred while processing your request.
+      </p>
+      <p class="text-gray-600">Please try again later.</p>
+    </div>
+  </main>
+</template>
